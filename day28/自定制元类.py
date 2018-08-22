6788fb89f4d7f8f21255e67716d0d0ceef8485d5
Administrator <winchook@gmail.com>
2018-08-22 17:54:06 +0800

    自定制元类

---
+++ day28/自定制元类.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by winchoo
+# 2018/8/22
+
+class MyType(type):
+    def __init__(self,a,b,c):
+        print('元类的构造函数执行')
+        # print(a)
+        # print(b)
+        # print(c)
+    def __call__(self, *args, **kwargs):
+        # print('=-======>')
+        # print(self)
+        # print(args,kwargs)
+        obj=object.__new__(self) #object.__new__(Foo)-->f1
+        self.__init__(obj,*args,**kwargs)  #Foo.__init__(f1,*arg,**kwargs)
+        return obj
+class Foo(metaclass=MyType): #Foo=MyType(Foo,'Foo',(),{})---》__init__
+    def __init__(self,name):
+        self.name=name #f1.name=name
+# print(Foo)
+# f1=Foo('alex')
+# print(f1)
+
+f1=Foo('winchoo')
+print(f1)
+print(f1.__dict__)
