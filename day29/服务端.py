e241ad9e6800239787255661c9d16b7af537c18f
Administrator <winchook@gmail.com>
2018-09-04 23:04:46 +0800

    描述符定义

--- day29/服务端.py
+++ day29/服务端.py
@@ -9,7 +9,7 @@
 import socket
 
 phone = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#买手机
-phone.bind(('127.0.0.1',8000))#绑定手机卡
+phone.bind(('192.168.0.188',8000))#绑定手机卡
 phone.listen(5)#开机，相当于半链接池
 print('---->')
 conn,addr=phone.accept()#等电话
