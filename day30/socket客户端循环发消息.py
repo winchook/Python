4e76de0123b468880cf54b8dedccb3b26e6fbcf5
Administrator <winchook@gmail.com>
2018-09-07 17:03:04 +0800

    socket服务端及客户端循环收发消息

---
+++ day30/socket客户端循环发消息.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by winchoo
+# 2018/9/7
+
+import socket
+from socket import *#虽然不提倡这种做法，但是在socket中比较特殊，可以这样使用
+
+ip_port = ('127.0.0.1',8080)
+back_log = 5
+buffer_size = 1024
+
+tcp_client = socket(AF_INET,SOCK_STREAM)
+tcp_client.connect(ip_port)
+
+while True:
+    msg = input('>>>:').strip()#使用用户输入的方式
+    tcp_client.send(msg.encode('utf-8'))
+    #tcp_client.send('hello'.encode('utf-8'))#这种方式写的固定了
+    print('客户端已经发送消息')
+    data = tcp_client.recv(buffer_size)#注意这块收发的时候只能是字节格式
+    print('收到服务端发来的消息：',data.decode('utf-8'))
+
+tcp_client.close()
---
+++ day30/socket服务端循环收消息.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by winchoo
+# 2018/9/7
+
+import socket
+from socket import *#虽然不提倡这种做法，但是在socket中比较特殊，可以这样使用
+
+ip_port = ('127.0.0.1',8080)
+back_log = 5
+buffer_size = 1024
+
+tcp_server = socket(AF_INET,SOCK_STREAM)
+tcp_server.bind(ip_port)
+tcp_server.listen(back_log)
+
+print('服务端开始运行了')
+conn,addr = tcp_server.accept()#运行后，这块会卡住，因为accept要等一个TCP链接进来
+print('双向链接是：',conn)
+print('客户端地址：',addr)
+
+while True:#实现循环收发消息
+    data = conn.recv(buffer_size)
+    print('客户端发来的消息是：',data.decode('utf-8'))
+    conn.send(data.upper())
+
+conn.close()
+tcp_server.close()
