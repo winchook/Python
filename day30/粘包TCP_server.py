df6bd59572fbbb0b67cb65cc67cd5a133ca479e6
Administrator <winchook@gmail.com>
2018-09-08 20:30:22 +0800

    限制缓冲区大小实现一次发一次收

--- day30/粘包TCP_server.py
+++ day30/粘包TCP_server.py
@@ -9,11 +9,11 @@ tcp_server.listen(back_log)
 
 conn,addr=tcp_server.accept()
 
-data1=conn.recv(buffer_size)
+data1=conn.recv(5)#这里的10表示一次只收取10个字节，所以会按照每次的收取大小来执行，如果设置较大的数字例如1024，则会一次性收取完客户端所传入的数据
 print('第1次数据',data1)
 
-data2=conn.recv(buffer_size)
+data2=conn.recv(5)
 print('第2次数据',data2)
 
-data3=conn.recv(buffer_size)
+data3=conn.recv(5)
 print('第3次数据',data3)
