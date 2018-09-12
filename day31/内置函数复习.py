0980daf9a182a36fe215ba107fada12f63b8d3b1
Administrator <winchook@gmail.com>
2018-09-12 15:54:31 +0800

    内置函数添加注释

--- day31/内置函数复习.py
+++ day31/内置函数复习.py
@@ -3,21 +3,28 @@
 # Created by winchoo
 # 2018/9/12
 
-# 三元运算
+# 三元运算:（三目运算），是对简单的条件语句的缩写
+#result = 值1 if 条件 else 值2
+#如果条件成立，那么将 “值1” 赋值给result变量，否则，将“值2”赋值给result变量
 a = 1
 result = "winchoo" if a == 1 else "chason"
 print(result)#winchoo
 
-# lambda 表达式
-
+# lambda 表达式:对于简单的函数，存在一种简便的表示方式，即：lambda表达式
+######## 普 通 函 数 ########
+# 定义函数（普通方式）
 def func(arg):
     return arg + 1
-print(func(2))#3
+# 执行函数
+result = func(123)
+######## lambda 表 达 式 ########
+# 定义函数（lambda表达式）
+my_lambda = lambda arg: arg + 1  # 相当于返回值
+# 执行函数
+result = my_lambda(123)
+print(result)
 
-func = lambda *args : print(args)
-print(func(1,2,3))
-
-# map
+# map:遍历序列，对序列中每个元素进行操作，最终获取新的序列
 li = [1,2,3,4,5]
 def func(s):
      return s + 1
@@ -26,7 +33,7 @@ print(list(ret))
 
 # print(list(map(lambda li : li + 1 ,li)))
 
-# filter 过滤
+# filter:对于序列中的元素进行筛选，最终获取符合条件的序列
 
 li = [22,33,44,55]
 
@@ -38,7 +45,7 @@ print(list(ret))
 
 print(list(filter(lambda s : s >33 ,li)))
 
-# reduce
+# reduce:对于序列内所有元素进行累计操作
 
 from functools import reduce
 
