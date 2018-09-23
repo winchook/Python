c8c01fc687350823892665b844e79b16534c447c
Administrator <winchook@gmail.com>
2018-09-23 21:21:51 +0800

    server调用html文件

--- day37/html_server.py
+++ day37/html_server.py
@@ -5,6 +5,38 @@
 
 #B/S模式的server端，实现浏览器与服务端交互,这里的client端就是浏览器
 
+# import socket
+#
+# #创建socket对象
+# def main():
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     sock.bind(('localhost',8089))
+#     sock.listen(5)
+#
+#     while True:
+#         #接收一个socket对象
+#         connection, address = sock.accept()#accept() -> (socket object, address info)
+#         #拿到对方的connection,得到数据
+#         buf = connection.recv(1024)
+#         #发送内容
+#         connection.sendall(bytes("HTTP/1.1 201 OK\r\n\r\n","utf8"))#浏览器http协议
+#         connection.sendall(bytes("<h1>Hello,World</h1>","utf8"))
+#         #关闭,继续接收其他的请求
+#         connection.close()
+#
+# if __name__ == '__main__':
+
+#首先,执行main()函数
+#    main()
+
+#验证
+#启动该服务端,打开浏览器输入http://127.0.0.1:8089/,即可看到Hello,World
+
+
+
+
+
+#将bytes("<h1>Hello,World</h1>","utf8")里面的标签写入到一个html文件中，然后读取
 import socket
 
 #创建socket对象
@@ -20,19 +52,16 @@ def main():
         buf = connection.recv(1024)
         #发送内容
         connection.sendall(bytes("HTTP/1.1 201 OK\r\n\r\n","utf8"))#浏览器http协议
-        connection.sendall(bytes("<h1>Hello,World</h1>","utf8"))
+
+        #读取tag.html内容
+        f = open("tag.html",'rb')
+        data = f.read()
+        # 发送内容
+        connection.sendall(data)
+
         #关闭,继续接收其他的请求
         connection.close()
 
 if __name__ == '__main__':
-
-#首先,执行main()函数
+    # 首先,执行main()函数
     main()
-
-#验证
-#启动该服务端,打开浏览器输入http://127.0.0.1:8089/,即可看到Hello,World
-
-
-
-
-
