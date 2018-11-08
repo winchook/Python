82a073978355eaa32f7897a00e65f0dee6d9002f
Administrator <winchook@gmail.com>
2018-11-08 16:14:05 +0800

    Django ORM对单表的增加方式二操作

--- day51/Django_ORM/app01/views.py
+++ day51/Django_ORM/app01/views.py
@@ -7,7 +7,13 @@ def index(request):
     return render(request,"index.html")
 
 def addbook(request):
+    # 第一种方式创建
+    # b=Book(name="python全栈开发",price=99,author="winchoo",pub_date="2018-11-08")
+    # b.save()
 
-    b=Book(name="python全栈开发",price=99,author="winchoo",pub_date="2018-11-08")
-    b.save()
+    # 第二种方式创建
+    Book.objects.create(name="python全栈开发",price=22,author="winchoo",pub_date="2018-11-08")
+    # 2018-11-08注意这里必须是日期格式，不能是字符串20181108，否则会报错
+    # ["'20181108' value has an invalid date format. It must be in YYYY-MM-DD format."]
+
     return HttpResponse("添加成功")
