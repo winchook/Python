577886ccee50b0a4f8fe12d25fe21be73f1caa6d
Administrator <winchook@gmail.com>
2018-11-10 23:52:08 +0800

    django 自定义admin样式

--- day53/ORM_multi/app01/admin.py
+++ day53/ORM_multi/app01/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 from app01 import models
 # Register your models here.
 
+# 给Book表加一个定制
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id','name','price','pub_date')#字段名定制
+    list_per_page = 2#分页定制
+    list_editable = ('name','price')#字段值可编辑定制
+    filter_horizontal = ('authors',)
+    search_fields = ('id','name','publish__name')#搜索定制
+    list_filter = ('pub_date','publish')#过滤条件定制
 
 # 注册medel类到admin的方式
 admin.site.register(models.Author)
-admin.site.register(models.Book)
+admin.site.register(models.Book,BookAdmin)
 admin.site.register(models.Publish)
 
 
