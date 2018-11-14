4876e971c263d48d492d460d7d55dbd5f7ae3d95
Administrator <winchook@gmail.com>
2018-11-14 17:13:33 +0800

    ORM基本操作的补充

--- day54/Django_demo1/app01/models.py
+++ day54/Django_demo1/app01/models.py
@@ -16,23 +16,12 @@ class Teachers(models.Model):
     """
     name = models.CharField(max_length=32)
 
-
-#自己手动创建第三张表来保存班级表及老师表的关系
-# class C2T(models.Model):
-# #     """
-# #     cid_id tid_id
-# #     1   1
-# #     1   2
-# #     6   1
-# #     要求cid只能是Classes表中的，而tid只能是Teachers表中的
-# #     """
-# #     cid = models.ForeignKey(Classes)
-# #     tid = models.ForeignKey(Teachers)
-
-#在Django中提供了ManyToManyField来自动创建的，加在Classes或者Teachers中都可以
-
 class Student(models.Model):
+    """
+    学生表
+    """
     username =  models.CharField(max_length=32)
     age = models.IntegerField()
     gender = models.BooleanField()
-    cs = models.ForeignKey(Classes,on_delete=models.CASCADE)
+    cs = models.ForeignKey(Classes,on_delete=models.CASCADE)#cs代表的是Classes表里面的一行数据,既有id又有name
+
