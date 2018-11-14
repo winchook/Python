2eb49cc494b8a23b191c64eb9b8ba4f3cb88dfb1
Administrator <winchook@gmail.com>
2018-11-14 22:33:28 +0800

    Django项目学员管理-为班级分配老师

--- day54/Django_demo1/app01/views/classes.py
+++ day54/Django_demo1/app01/views/classes.py
@@ -3,6 +3,8 @@ from app01 import models
 
 def get_classes(request):
     cls_list = models.Classes.objects.all()
+    for item in cls_list:
+        print(item.id, item.title, item.m.all())
     return render(request,'get_classes.html',{'cls_list':cls_list})
 
 def add_classes(request):
@@ -28,4 +30,25 @@ def edit_classes(request):
         nid = request.GET.get('nid')
         title = request.POST.get('xxoo')
         models.Classes.objects.filter(id=nid).update(title=title)
+        return redirect('/classes.html')
+
+def set_teacher(request):
+    if request.method == 'GET':
+        nid = request.GET.get('nid')
+        cls_obj = models.Classes.objects.filter(id=nid).first()
+        cls_teacher_list = cls_obj.m.all().values_list('id','name')
+        #[(1,'winchoo),(2,'chason')]元组
+        #list(zip(*v)) --> [(1,2),('winchoo','chason')]
+        # 拿到当前班级任课教师的id列表
+        id_list = list(zip(*cls_teacher_list))[0] if list(zip(*cls_teacher_list)) else []
+
+        all_teacher_list = models.Teachers.objects.all()
+        return render(request,'set_teacher.html',{'id_list':id_list,'all_teacher_list':all_teacher_list,'nid':nid})
+    elif request.method == 'POST':
+        nid = request.GET.get('nid')
+        ids = request.POST.getlist('teacher_ids')
+        print('当前班级的ID',nid,'分配老师的ID',ids)
+
+        obj = models.Classes.objects.filter(id=nid).first()
+        obj.m.set(ids)
         return redirect('/classes.html')
