51aa1e226bbd7f3fe7e9c000dedd1a8334c7050b
Administrator <winchook@gmail.com>
2018-11-23 19:01:18 +0800

    Django admin创建表操作

--- day61/Django_UploadFiles/app01/admin.py
+++ day61/Django_UploadFiles/app01/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from app01 import models
+admin.site.register(models.UserInfo)
--- day61/Django_UploadFiles/app01/models.py
+++ day61/Django_UploadFiles/app01/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    username = models.CharField(
+        null = True,
+        db_column='user',#自定义列名
+        verbose_name='用户名',#Admin中显示的字段名称
+        help_text='请输入用户名',#Admin中该字段的提示信息
+        max_length=12,
+        db_index=True,#只能加速查找
+        # unique=True,#加速查找;限制列值唯一,保证列是唯一的
+        # primary_key=True,#数据库中字段是否为主键(不能为空)
+    )
+
+    user_type_choices = [
+        (0,'普通用户'),
+        (1,'超级用户'),
+        (2,'VIP用户'),
+    ]
+
+    user_type = models.IntegerField(
+        choices=user_type_choices,#Admin中显示选择框的内容，用不变动的数据放在内存中从而避免跨表操作
+    )
