2d96f8bf2e08bec1b770819e8ff7c602df113a4b
Administrator <winchook@gmail.com>
2018-11-24 21:27:31 +0800

    Django-model指定外键约束字段参数

--- day61/Django_UploadFiles/app01/admin.py
+++ day61/Django_UploadFiles/app01/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from app01 import models
-admin.site.register(models.UserInfo)
+# admin.site.register(models.UserInfo)
+admin.site.register(models.Part)
+admin.site.register(models.User)
--- day61/Django_UploadFiles/app01/models.py
+++ day61/Django_UploadFiles/app01/models.py
@@ -2,24 +2,43 @@ from django.db import models
 
 # Create your models here.
 
-class UserInfo(models.Model):
-    username = models.CharField(
-        null = True,
-        db_column='user',#自定义列名
-        verbose_name='用户名',#Admin中显示的字段名称
-        help_text='请输入用户名',#Admin中该字段的提示信息
-        max_length=12,
-        db_index=True,#只能加速查找
-        # unique=True,#加速查找;限制列值唯一,保证列是唯一的
-        # primary_key=True,#数据库中字段是否为主键(不能为空)
-    )
+# class UserInfo(models.Model):
+#     username = models.CharField(
+#         null = True,
+#         db_column='user',#自定义列名
+#         verbose_name='用户名',#Admin中显示的字段名称
+#         help_text='请输入用户名',#Admin中该字段的提示信息
+#         max_length=12,
+#         db_index=True,#只能加速查找
+#         # unique=True,#加速查找;限制列值唯一,保证列是唯一的
+#         # primary_key=True,#数据库中字段是否为主键(不能为空)
+#     )
+#
+#     user_type_choices = [
+#         (0,'普通用户'),
+#         (1,'超级用户'),
+#         (2,'VIP用户'),
+#     ]
+#
+#     user_type = models.IntegerField(
+#         choices=user_type_choices,#Admin中显示选择框的内容，用不变动的数据放在内存中从而避免跨表操作
+#     )
 
-    user_type_choices = [
-        (0,'普通用户'),
-        (1,'超级用户'),
-        (2,'VIP用户'),
-    ]
+# class SomeBody(models.Model):
+#     caption = models.CharField(max_length=12)
+#     pk = models.ForeignKey(
+#         to = "UserInfo",#要进行关联的表名
+#         to_field='id',#要关联的表中的字段名称
+#         related_query_name='b',# 反向操作时，使用的连接前缀，用于替换【表名】     如： models.UserGroup.objects.filter(表名__字段名=1).values('表名__字段名')
+#     )
 
-    user_type = models.IntegerField(
-        choices=user_type_choices,#Admin中显示选择框的内容，用不变动的数据放在内存中从而避免跨表操作
-    )
+class User(models.Model):
+    username = models.CharField(max_length=32,db_index=True),
+    par = models.ForeignKey(
+        to='Part',
+        to_field='id',
+        on_delete=models.CASCADE,
+    ),
+
+class Part(models.Model):
+    caption = models.CharField(max_length=21),
