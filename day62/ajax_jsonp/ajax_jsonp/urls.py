a58655dfaf1268a4e158b6dede9711cb42e7f032
Administrator <winchook@gmail.com>
2018-11-25 17:59:08 +0800

    Ajax XmlHttpRequest对象及Jsonp实现跨域

---
+++ day62/ajax_jsonp/ajax_jsonp/urls.py
@@ -0,0 +1,29 @@
+"""ajax_jsonp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from app01 import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^index.html$', views.index),
+    url(r'^ajax1.html$', views.ajax1),
+    url(r'^upload.html$', views.upload),
+    url(r'^upload_img.html$', views.upload_img),
+    url(r'^jsonp.html$', views.jsonp),
+    url(r'^ajax3.html$', views.ajax3),
+]
